c00a6ec05273f3e39ee943014a5dff7740c6062b
OMOTOLA OLASOPE <tola.olasope@gmail.com>
2023-10-16 19:24:45 +0100

    update post-route

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "https://9lm9fefr2d.execute-api.eu-west-1.amazonaws.com/default/post-route",
+                url: "https://lv94jdn7gg.execute-api.eu-west-1.amazonaws.com/omotolas_function/post-route",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
